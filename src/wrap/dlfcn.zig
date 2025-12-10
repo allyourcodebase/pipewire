dbc95b806f47fb7fe17c98d7296b9b60e3404aec
Mason Remaley <mason@anthropicstudios.com>
2025-12-10 15:30:03 -0800

    Adds assert that handles don't collide with built in handles

--- src/wrap/dlfcn.zig
+++ src/wrap/dlfcn.zig
@@ -99,8 +99,13 @@ pub const Lib = struct {
         try writer.print("@\"{f}\"", .{std.zig.fmtString(self.name)});
     }
 
-    fn sym(val: anytype) *anyopaque {
-        return @ptrCast(@constCast(val));
+    fn sym(val: anytype) ?*anyopaque {
+        const result: ?*anyopaque = @ptrCast(@constCast(val));
+        // In practice these constants are defined to be values like `0` and `-1` that won't ever
+        // collide with real pointers.
+        comptime std.debug.assert(result != c.RTLD_NEXT);
+        comptime std.debug.assert(result != c.RTLD_DEFAULT);
+        return result;
     }
 };
 
