232e268af0adb945a58f927685b738154aedeaed
Mason Remaley <mason@anthropicstudios.com>
2025-12-12 18:09:28 -0800

    Disables SSE41 in audioconvert, see #10

--- build.zig
+++ build.zig
@@ -257,16 +257,17 @@ pub fn build(b: *std.Build) void {
                         .flags = flags,
                     });
                 }
-                if (target.result.cpu.has(.x86, .sse4_1)) {
-                    audioconvert.root_module.addCMacro("HAVE_SSE41", "1");
-                    audioconvert.root_module.addCSourceFiles(.{
-                        .root = upstream.path("spa/plugins/audioconvert"),
-                        .files = &.{
-                            "fmt-ops-sse41.c",
-                        },
-                        .flags = flags,
-                    });
-                }
+                // https://github.com/allyourcodebase/pipewire/issues/10
+                // if (target.result.cpu.has(.x86, .sse4_1)) {
+                //     audioconvert.root_module.addCMacro("HAVE_SSE41", "1");
+                //     audioconvert.root_module.addCSourceFiles(.{
+                //         .root = upstream.path("spa/plugins/audioconvert"),
+                //         .files = &.{
+                //             "fmt-ops-sse41.c",
+                //         },
+                //         .flags = flags,
+                //     });
+                // }
                 if (target.result.cpu.has(.x86, .avx) and target.result.cpu.has(.x86, .fma)) {
                     // Upstream build system also only defines either if both are present
                     audioconvert.root_module.addCMacro("HAVE_AVX", "1");
